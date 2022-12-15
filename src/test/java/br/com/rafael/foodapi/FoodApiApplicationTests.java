bc2fa779eda1749ad9822337792e5854e530edee
Rafael Sá <sanunesrafael@gmail.com>
2022-12-15 08:56:55 -0300

    criando projeto

---
+++ src/test/java/br/com/rafael/foodapi/FoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.foodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
